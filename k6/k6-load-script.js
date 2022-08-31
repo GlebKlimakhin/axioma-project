cd4f69971e288226a2234285a731c6c11b58553a
Gleb Klimakhin <klimakhin.fortech@gmail.com>
2022-08-31 11:49:37 +0300

    nginx config works

--- k6/k6-load-script.js
+++ k6/k6-load-script.js
@@ -3,9 +3,9 @@ import {sleep} from 'k6'
 
 export const options = {
     stages : [
-        {duration : '3m', target: 200},
-        {duration : '3m', target: 600},
-        {duration : '2m', target : 0}
+        {duration : '5m', target: 100},
+        {duration : '5m', target: 200},
+        {duration : '10m', target : 0}
     ],
     thresholds : {
         http_req_duration: ['p[99] < 150']
