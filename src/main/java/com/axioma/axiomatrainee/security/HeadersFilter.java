a5301515cd1d668fdabde96e202b8335c3ed462e
GlebKlimakhin <klimakhiny@mail.ru>
2022-09-03 21:51:28 +0300

    security config

--- src/main/java/com/axioma/axiomatrainee/security/HeadersFilter.java
+++ src/main/java/com/axioma/axiomatrainee/security/HeadersFilter.java
@@ -1,5 +1,7 @@
 package com.axioma.axiomatrainee.security;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class HeadersFilter implements Filter {
 
     @Override
