8749bae0c53f7ca6da304295e7f56ae78fe3697f
GlebKlimakhin <klimakhiny@mail.ru>
2022-09-03 20:48:56 +0300

    security config

--- src/main/java/com/axioma/axiomatrainee/security/HeadersFilter.java
+++ src/main/java/com/axioma/axiomatrainee/security/HeadersFilter.java
@@ -1,6 +1,7 @@
 package com.axioma.axiomatrainee.security;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -8,27 +9,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class HeadersFilter implements Filter {
+public class HeadersFilter extends OncePerRequestFilter {
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest,
-                         ServletResponse servletResponse,
-                         FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.addHeader("Access-Control-Allow-Origin", "*");
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
         response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         filterChain.doFilter(request, response);
-        }
-
-    @Override
-    public void destroy() {
-
     }
+
+
 }
