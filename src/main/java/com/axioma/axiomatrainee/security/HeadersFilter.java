886758bef624a47805598e915eba666987d9e09b
GlebKlimakhin <klimakhiny@mail.ru>
2022-09-03 21:55:02 +0300

    security config

--- src/main/java/com/axioma/axiomatrainee/security/HeadersFilter.java
+++ src/main/java/com/axioma/axiomatrainee/security/HeadersFilter.java
@@ -3,6 +3,7 @@ package com.axioma.axiomatrainee.security;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -11,29 +12,27 @@ import java.io.IOException;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class HeadersFilter implements Filter {
+public class HeadersFilter implements OncePerRequestFilter {
+
+
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest,
+//                         ServletResponse servletResponse,
+//                         FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        HttpServletResponse response = (HttpServletResponse) servletResponse;
+//        response.addHeader("Access-Control-Allow-Origin", "*");
+//        response.addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//        response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//        filterChain.doFilter(request, response);
+//        }
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest,
-                         ServletResponse servletResponse,
-                         FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.addHeader("Access-Control-Allow-Origin", "*");
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
         response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         filterChain.doFilter(request, response);
-        }
-
-
-
-    @Override
-    public void destroy() {
-
     }
 }
