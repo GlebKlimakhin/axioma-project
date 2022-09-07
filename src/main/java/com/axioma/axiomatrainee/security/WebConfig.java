2141c0fdb81df940f87174344bfeff73be1dd4df
GlebKlimakhin <klimakhiny@mail.ru>
2022-09-08 01:11:04 +0300

    cors options enable

--- src/main/java/com/axioma/axiomatrainee/security/WebConfig.java
+++ src/main/java/com/axioma/axiomatrainee/security/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedOrigins("*")
                 .allowedHeaders("*");
     }
 }
