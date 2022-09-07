3f9513b2f744fa7fcc90e1953dfa5e180535b54c
GlebKlimakhin <klimakhiny@mail.ru>
2022-09-07 22:56:23 +0300

    dto

--- src/main/java/com/axioma/axiomatrainee/requestdto/CreateGroupRequestDto.java
+++ src/main/java/com/axioma/axiomatrainee/requestdto/CreateGroupRequestDto.java
@@ -1,9 +1,9 @@
 package com.axioma.axiomatrainee.requestdto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @NoArgsConstructor
@@ -13,6 +13,9 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CreateGroupRequestDto {
 
+    @NotBlank
     String name;
+
+    @NonNull
     List<Long> userIds;
 }
