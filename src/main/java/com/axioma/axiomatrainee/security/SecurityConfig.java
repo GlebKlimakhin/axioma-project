c82a3976350ce948fa599662a8041303ea388b0c
GlebKlimakhin <klimakhiny@mail.ru>
2022-09-08 23:46:48 +0300

    cors

--- src/main/java/com/axioma/axiomatrainee/security/SecurityConfig.java
+++ src/main/java/com/axioma/axiomatrainee/security/SecurityConfig.java
@@ -32,11 +32,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedMethods(List.of("PUT", "POST", "OPTIONS", "GET", "PATCH"));
-        corsConfiguration.setAllowedHeaders(List.of("Content-Type", "Accept", "Origin"));
-        source.registerCorsConfiguration("/**", corsConfiguration);
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
