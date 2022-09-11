7a143c52ca304cee9490f65675627b7d72cebe59
GlebKlimakhin <klimakhiny@mail.ru>
2022-09-11 15:47:23 +0300

    cors filter

--- src/main/java/com/axioma/axiomatrainee/security/SimpleCorsFilter.java
+++ src/main/java/com/axioma/axiomatrainee/security/SimpleCorsFilter.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsFilter extends OncePerRequestFilter {
+public class SimpleCorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
