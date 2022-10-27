5078a49136314b49bc921a5162e909d1291f83e7
Gleb Klimakhin <klimakhin.fortech@gmail.com>
2022-10-27 15:32:29 +0300

    homework scheduler

---
+++ src/main/java/com/axioma/axiomatrainee/service/scheduler/HomeworkScheduler.java
@@ -0,0 +1,31 @@
+package com.axioma.axiomatrainee.service.scheduler;
+
+import com.axioma.axiomatrainee.repository.IHomeworkRepository;
+import com.axioma.axiomatrainee.utill.DateParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class HomeworkScheduler {
+
+    private final IHomeworkRepository homeworkRepository;
+
+    private static final long minusMonthTimeUnix = System.currentTimeMillis() - 30 * 86400;
+
+
+    @Scheduled(fixedRate = 86400000)
+    @Transactional
+    public void deleteDoneHomeworks() {
+        long nowTimeUnix = System.currentTimeMillis();
+        homeworkRepository.findAll()
+                .stream()
+                .filter(h ->
+                    DateParser.parseFromDate(h.getExpirationDate()) <= minusMonthTimeUnix)
+                .forEach(homeworkRepository::delete);
+    }
+}
